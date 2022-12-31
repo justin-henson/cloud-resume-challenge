ed3f65ec67a5c5b03c0e18f422a1eb5f38239093
justin-henson <justin.henson@proton.me>
2022-12-31 14:27:39 -0500

    updates

--- serverless-architecture-with-SAM/tests/test_handler.py
+++ serverless-architecture-with-SAM/tests/test_handler.py
@@ -5,6 +5,8 @@ from unittest import mock
 
 from hello_world import app
 
+os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
+
 with open('serverless-architecture-with-SAM/template.yaml', 'r') as f:
     TABLENAME = re.search(r'TableName: (.*)?', f.read()).group(1)
 
