651736a8845b81d21d2d1d937f87fc2c43422df8
justin-henson <justin.henson@proton.me>
2022-12-31 16:02:49 -0500

    updates

--- serverless-architecture-with-SAM/hello_world/app.py
+++ serverless-architecture-with-SAM/hello_world/app.py
@@ -3,7 +3,7 @@ import boto3
 
 dynamodb = boto3.resource('dynamodb', region_name="us-east-1")
 
-table = dynamodb.Table('resume-website-app-tbl', region_name="us-east-1")
+table = dynamodb.Table('resume-website-app-tbl')
 
 def lambda_handler(event, context):
     response = table.get_item(
