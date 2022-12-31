bd3f5d4d9c2d812ec624a16e0a9a9f63c11d8ab8
justin-henson <justin.henson@proton.me>
2022-12-31 15:36:25 -0500

    updates

--- serverless-architecture-with-SAM/hello_world/app.py
+++ serverless-architecture-with-SAM/hello_world/app.py
@@ -1,7 +1,11 @@
 import json
 import boto3
 
-dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+dynamodb = boto3.resource('dynamodb', 
+aws_access_key_id=AWS_ACCESS_KEY_ID, 
+aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+region_name=AWS_DEFAULT_REGION))
+
 table = dynamodb.Table('resume-website-app-tbl')
 
 def lambda_handler(event, context):
