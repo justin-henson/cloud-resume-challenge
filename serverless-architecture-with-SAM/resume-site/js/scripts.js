9acdad81c00e78901b2e5305ae67c0c58cb2f781
justin-henson <justin.henson@proton.me>
2023-01-05 10:11:09 -0500

    fix visitor counter

--- serverless-architecture-with-SAM/resume-site/js/scripts.js
+++ serverless-architecture-with-SAM/resume-site/js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
     $.post('https://pl8h7bxm3j.execute-api.us-east-1.amazonaws.com/Prod/visit/')
     .done(visitor_counter => {
         $('#loader').hide();
-        $('#visits').text(visitor_counter);
+        $('#visits').text(visitor_counter.visit_count);
     })
     .fail(e => {
         console.log('Error');
