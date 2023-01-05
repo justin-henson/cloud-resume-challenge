c32d0884122984d74844db628fe77dda10128943
justin-henson <justin.henson@proton.me>
2023-01-05 08:28:49 -0500

    updates

--- serverless-architecture-with-SAM/resume-site/js/scripts.js
+++ serverless-architecture-with-SAM/resume-site/js/scripts.js
@@ -1,7 +1,7 @@
 "use strict";
 
 $(document).ready(() => {
-    $.post('https://pl8h7bxm3j.execute-api.us-east-1.amazonaws.com')
+    $.post('https://pl8h7bxm3j.execute-api.us-east-1.amazonaws.com/Prod/visit/')
     .done(visitor_counter => {
         $('#loader').hide();
         $('#visits').text(visitor_counter);
